886c3b807ef06afc709631e73c33c578518545aa
CodeHz <CodeHz@outlook.com>
2016-01-09 14:47:40 +0800

    create Models(Article, User)
    Signed-off-by: CodeHz <CodeHz@outlook.com>

---
+++ models/User.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = function (mongoose) {
+    let bcrypt = require('bcrypt'), Schema = mongoose.Schema;
+    var userSchema = new Schema({
+        id:         Number,
+        name:       { type: String, required: true },
+        password:   { type: String, required: true },
+        phone:      String,
+        email:      { type: String, required: true, index: { unique: true }},
+        permission: Number
+    });
+    
+    userSchema.pre('save', next => {
+        let user = this;
+        if (!user.isModified('password')) return next();
+        bcrypt.hash(user.password, bcrypt.genSaltSync(), (err, hash) => {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+    
+    userSchema.methods.comparePassword = (passwordToCompareWith, next) => {
+        bcrypt.compare(passwordToCompareWith, this.password, (err, valid) => {
+            if (err) return next (err);
+            next(null, valid);
+        });
+    };
+    return mongoose.model('User', userSchema);
+}
